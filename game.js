38b5cf05f4347309249d084d7482555e79810891
William Edwards <wil@Williams-MacBook-Pro.local>
2014-08-23 14:03:10 +0200

    tidy up world map

--- game.js
+++ game.js
@@ -7,16 +7,18 @@ var world_map = {
 	vbo: gl.createBuffer(),
 	pMatrix: mat4_identity,
 	mvMatrix: mat4_identity,
+	start_time: null,
 	program: Program(
 		"precision mediump float;\n"+
+		"#define PI 3.1415926535897932384626433832795\n"+ // madness!
 		"attribute vec2 vertex;\n"+
 		"uniform mat4 mvMatrix, pMatrix;\n"+
+		"uniform float t;\n"+
 		"void main() {\n"+
 		"	float lat = vertex.y, lng = vertex.x;\n"+
-		"	float x = -cos(lat)*cos(lng);\n"+
-		"	float y = sin(lat);\n"+
-		"	float z = cos(lat)*sin(lng);\n"+
-		"	gl_Position = pMatrix * mvMatrix * vec4(x,y,z,1.0);\n"+
+		"	vec3 globe = vec3(-cos(lat)*cos(lng),sin(lat),cos(lat)*sin(lng));\n"+
+		"	vec3 merc = vec3(lng, 180.0/PI * log(tan(PI/4.0+lat*(PI/180.0)/2.0)), 0);\n"+
+		"	gl_Position = pMatrix * mvMatrix * vec4(mix(globe,merc,t),1.0);\n"+
 		"}\n",
 		"precision mediump float;\n"+
 		"uniform vec4 colour;\n"+
@@ -39,6 +41,7 @@ var world_map = {
 				pMatrix: this.pMatrix,
 				mvMatrix: this.mvMatrix,
 				colour: [0, 1, 0, 1],
+				t: Math.min(1, (now()-this.start_time) / 3000),
 			}, this);
 		var ofs = 0, parts = this.data.ofs;
 		for(var part=0; part<parts.length; part++) {
@@ -50,7 +53,6 @@ var world_map = {
 };
 
 function new_game() {
-	splash.dismiss();
 	loading = false;
 	onResize();
 }
@@ -63,6 +65,7 @@ function load_world_map() {
 		gl.bindBuffer(gl.ARRAY_BUFFER,world_map.vbo);
 		gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(world_map.data.pts),gl.STATIC_DRAW);
 		gl.bindBuffer(gl.ARRAY_BUFFER,null);
+		world_map.start_time = now();
 		return true;
 	}
 	return false;
@@ -77,5 +80,6 @@ function onResize() {
 }
 
 function render() {
+	gl.clear(gl.DEPTH_BIT|gl.COLOR_BIT);
 	world_map.draw();
 }
